1cca493d50bdb25aa0f7735188fee3487c8101a7
Orin10120 <rindang3001@gmail.com>
2024-03-23 09:15:58 +0700

    finish jurnal

--- Program.cs
+++ Program.cs
@@ -6,10 +6,42 @@ using System.Threading.Tasks;
 
 namespace Jurnal5
 {
-    internal class Program
+    public class simpleDatabase<T>
+    {
+        private List<T> storedData;
+        private List<DateTime> inputdates;
+
+        public simpleDatabase()
+        {
+            this.storedData = new List<T>();
+            this.inputdates = new List<DateTime>();
+        }
+
+        public void AddNewData(T data)
+        {
+            this.storedData.Add(data);
+            this.inputdates.Add(DateTime.Now);
+        }
+
+        
+        public void printAllData()
+        {
+            for (int i = 0; i < this.storedData.Count; i++)
+            {
+                Console.WriteLine("data: " + storedData[i] + " Waktu: " + inputdates);
+            }
+        }
+    }
+    
+    class Program
     {
         static void Main(string[] args)
         {
+            simpleDatabase<double> dataNew = new simpleDatabase<double>();
+            dataNew.AddNewData(13);
+            dataNew.AddNewData(22);
+            dataNew.AddNewData(30);
+            dataNew.printAllData();
         }
     }
 }
