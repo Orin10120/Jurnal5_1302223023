53d3352e0c6240918ac8cd4685640c05b1100ad0
Orin10120 <rindang3001@gmail.com>
2024-03-23 08:29:41 +0700

    implementasi generic method

--- Program.cs
+++ Program.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace Jurnal5
 {
-    internal class Program
+    public class simpleDatabase<T>
+    {
+        private List<T> storedData;
+        private List<DateTime> inputdates;
+
+        simpleDatabase(List<T> storedData, List<DateTime> inputdates)
+        {
+            this.storedData = storedData;
+            this.inputdates = inputdates;
+        }
+
+        public void AddNewData(T data, T inputDates)
+        {
+            this.storedData.Add(data);
+
+        }
+
+        public T JumlahTigaAngka(T angka1, T angka2, T angka3)
+        {
+            return angka1
+        }
+        
+        public void printAllData()
+        {
+            Console.WriteLine();
+        }
+    }
+    
+    class Program
     {
         static void Main(string[] args)
         {
